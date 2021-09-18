83b61b5012bfd4186f402cf5e4a04cf61b9487df
Anara Kulmambetova <anara.akimkanova.kk@gmail.com>
2021-09-18 09:41:07 +0400

    my 2nd home work in JawaScript

---
+++ script.js
@@ -0,0 +1,14 @@
+let lastName =prompt("Ваше фамилия ?");
+ 
+let userName = prompt ("Ваше имя ? ");
+ 
+let middleName= prompt("Ваше очества ?");
+
+let emailName = prompt("Ваше email ?");
+
+ alert ("Саламатсызбы? " + (lastName+" "+userName+ " " +middleName)+ "  Биздин командага кош келиниз!");
+
+ document.write("<b>ФИО: </b>"+lastName+" "+userName+" "+middleName+"<br>"+ "<b>email: </b>"+emailName+"<br>"+ 
+ "<b>Адрес:</b> P.O.Box 3311  Hamad Bin Rashid road, RAC building, Flat 5577 ");
+ 
+ 
